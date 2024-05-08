62abb4ae8d55cbd63966ca0d6e8745d2f15751fe
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-05-08 14:14:04 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogoPassLockerComponent } from './login/logo-pass-locker/logo-pass-locker.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoPassLockerComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/login/logo-pass-locker/logo-pass-locker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-pass-locker',
+  templateUrl: './logo-pass-locker.component.html',
+  styleUrl: './logo-pass-locker.component.css'
+})
+export class LogoPassLockerComponent {
+
+}
