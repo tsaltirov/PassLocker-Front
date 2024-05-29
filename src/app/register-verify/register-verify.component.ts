d9c020d18272862995c5f48e7ddbb8b8d439735c
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-05-29 17:33:12 -0500

    se corrigio un error

--- src/app/register-verify/register-verify.component.ts
+++ src/app/register-verify/register-verify.component.ts
@@ -18,8 +18,7 @@ export class RegisterVerifyComponent implements OnInit {
   public email: string;
   public password: string;
   public fullName: string;
-  public userType: string;}
-
+  public userType: string;
   constructor(private _route: ActivatedRoute, private router: Router, private _register: RegisterService) {
     this.email = '';
     this.password = '';
@@ -71,5 +70,9 @@ export class RegisterVerifyComponent implements OnInit {
       .catch(error => {
         console.error('Error al registrar el usuario', error);
       });
+
+
   }
+
+
 }
