8504196ac80306f5b802b89be77ff5e84f7283e0
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-05-29 12:40:17 -0500

    Se configuro el registro

--- src/app/login/formulario-login/formulario-login.component.ts
+++ src/app/login/formulario-login/formulario-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-formulario-login',
@@ -14,5 +15,18 @@ export class FormularioLoginComponent {
     this.password = ''
   }
 
-  
-}
+ async sendValuesLogin(){
+    try {
+      const response = await axios.post('http://localhost:3000/api/auth/login', {
+        email: this.email,
+        password: this.password
+      });
+
+      console.log('Login:', response.data);
+
+    } catch (error) {
+      console.error('Error al intentar logear', error);
+    }
+  }
+  }  
+
