3caf05112945f4ee6e85d93e7eed5d61a2b3251c
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-06-25 09:58:55 -0500

    add validations in modify-password-manager

--- src/app/modify-password/modify-password.component.ts
+++ src/app/modify-password/modify-password.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModdifyserviceService } from './services/moddifyservice.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modify-password',
@@ -9,56 +10,73 @@ import Swal from 'sweetalert2';
   styleUrls: ['./modify-password.component.css'],
 })
 export class ModifyPasswordComponent implements OnInit {
-  id: string = '';
-  nombreDelServicio: string = '';
-  nombreDelUsuario: string = '';
-  nueva: string = '';
 
-  constructor(private route: ActivatedRoute, private modifyService: ModdifyserviceService, private router:Router) {}
+  id: string = '';
+  modifyPasswordForm: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private modifyService: ModdifyserviceService,
+    private router: Router
+  ) {
+    this.modifyPasswordForm = this.fb.group({
+      nombreDelServicio: ['', Validators.required],
+      nombreDelUsuario: ['', Validators.required],
+      nueva: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
 
   ngOnInit(): void {
+
+
+
     this.route.paramMap.subscribe(params => {
       this.id = params.get('id') || '';
     });
   }
 
-  async modifyPassword() { 
-    const result = await Swal.fire({
-      title: '¿Estás seguro?',
-      text: "¿Deseas modificar la contraseña?",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Sí, modificarla',
-      cancelButtonText: 'Cancelar'
-    });
+  async modifyPassword() {
+    if (this.modifyPasswordForm.valid) {
+      const result = await Swal.fire({
+        title: '¿Estás seguro?',
+        text: "¿Deseas modificar la contraseña?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, modificarla',
+        cancelButtonText: 'Cancelar'
+      });
 
-    if (result.isConfirmed) {
-      try {
-        await this.modifyService.modifyPassword(this.id, this.nombreDelServicio, this.nombreDelUsuario, this.nueva);
-        Swal.fire(
-          'Modificada',
-          'La contraseña ha sido modificada.',
-          'success'
-        );
-        this.router.navigate(['/home']);
-      } catch (error) {
-        console.error('Error actualizando la contraseña:', error);
-        Swal.fire(
-          'Error',
-          'Hubo un problema al modificar la contraseña.',
-          'error'
-        );
-      
+      if (result.isConfirmed) {
+        try {
+          const { nombreDelServicio, nombreDelUsuario, nueva } = this.modifyPasswordForm.value;
+          await this.modifyService.modifyPassword(this.id, nombreDelServicio, nombreDelUsuario, nueva);
+          Swal.fire(
+            'Modificada',
+            'La contraseña ha sido modificada.',
+            'success'
+          );
+          this.router.navigate(['/home']);
+        } catch (error) {
+          console.error('Error actualizando la contraseña:', error);
+          Swal.fire(
+            'Error',
+            'Hubo un problema al modificar la contraseña.',
+            'error'
+          );
+        }
+      }
+    } else {
+      Swal.fire(
+        'Error',
+        'Por favor complete el formulario correctamente.',
+        'error'
+      );
     }
-    
-
   }
-}
-cancel() {
-    
- 
-  this.router.navigate(['/home']);
 
-}}
+  cancel() {
+    this.router.navigate(['/home']);
+  }}
